50186ba64845576dfc0410d3f571612c4f84ccb7
Osama33Abbas <s443011956@st.uqu.edu.sa>
2025-04-08 16:02:37 +0300

    routes fixed

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,27 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\PdfController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Book Controller
+Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+Route::get('/books/index', [BookController::class, 'index'])->name('books.index');
+Route::post('/books/store', [BookController::class, 'store'])->name('books.store');
+Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
+Route::post('/books/{book}/update', [BookController::class, 'update'])->name('books.update');
+Route::delete('/books/{book}/delete', [BookController::class, 'destroy'])->name('books.destroy');
+
+
+//PDF Controller
+Route::get('/books/{book}/download', [PdfController::class, 'downloadPdf'])->name('books.download');
+Route::get('/books/{book}/view', [PdfController::class, 'viewBookPdf'])->name('books.view');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -16,5 +32,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__.'/auth.php';
