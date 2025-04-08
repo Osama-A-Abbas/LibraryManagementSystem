d1a097b55c08e0097cc78dda10640846bc61c36d
Osama33Abbas <s443011956@st.uqu.edu.sa>
2025-04-08 15:59:26 +0300

    breez inital

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
-
-use App\Http\Controllers\PdfController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//Book Controller
-Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
-Route::get('/books/index', [BookController::class, 'index'])->name('books.index');
-Route::post('/books/store', [BookController::class, 'store'])->name('books.store');
-Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
-Route::post('/books/{book}/update', [BookController::class, 'update'])->name('books.update');
-Route::delete('/books/{book}/delete', [BookController::class, 'destroy'])->name('books.destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-//PDF Controller
-Route::get('/books/{book}/download', [PdfController::class, 'downloadPdf'])->name('books.download');
-Route::get('/books/{book}/view', [PdfController::class, 'viewBookPdf'])->name('books.view');
-
-
-// Route::get('/books', [BookController::class, 'index']);
-// Route::post('/books', [BookController::class, 'store']);
-// Route::get('/books/{id}', [BookController::class, 'show']);
-// Route::put('/books/{id}', [BookController::class, 'update']);
-// Route::delete('/books/{id}', [BookController::class, 'destroy']);
+require __DIR__.'/auth.php';
