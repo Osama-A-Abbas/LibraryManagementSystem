eee17b1824c6c74a907f98fa41b0785842084dae
Osama33Abbas <s443011956@st.uqu.edu.sa>
2025-04-08 15:48:28 +0300

    sm

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Book Controller
 Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
 Route::get('/books/index', [BookController::class, 'index'])->name('books.index');
 Route::post('/books/store', [BookController::class, 'store'])->name('books.store');
@@ -16,8 +17,11 @@ Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.e
 Route::post('/books/{book}/update', [BookController::class, 'update'])->name('books.update');
 Route::delete('/books/{book}/delete', [BookController::class, 'destroy'])->name('books.destroy');
 
+
+//PDF Controller
 Route::get('/books/{book}/download', [PdfController::class, 'downloadPdf'])->name('books.download');
 Route::get('/books/{book}/view', [PdfController::class, 'viewBookPdf'])->name('books.view');
+
 
 // Route::get('/books', [BookController::class, 'index']);
 // Route::post('/books', [BookController::class, 'store']);
